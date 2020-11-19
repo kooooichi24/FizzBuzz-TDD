8ce56893ad357e169f7fb3b21607077dc9f9e6a3
古川広一 <ex.kouichi1224@gmail.com>
2020-11-20 01:29:23 +0900

    JUnit5の環境を整えて、failTestが失敗することを確認

---
+++ src/test/java/tddbc/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package tddbc;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FizzBuzzTest {
+    @Test
+    void test() {
+        fail("これは失敗するはず");
+    }
+}
