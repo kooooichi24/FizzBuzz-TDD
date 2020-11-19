4558f11f857c6921d0e3526b9d09e4e4bf36f6bf
古川広一 <ex.kouichi1224@gmail.com>
2020-11-20 01:59:13 +0900

    数を文字列に変換する の正常系が完了

---
+++ src/main/java/tddbc/FizzBuzz.java
@@ -0,0 +1,10 @@
+package tddbc;
+
+/**
+ * FizzBuzz を実現するためのクラス
+ */
+public class FizzBuzz {
+    public String convert(int num) {
+        return String.valueOf(num);
+    }
+}
--- src/test/java/tddbc/FizzBuzzTest.java
+++ src/test/java/tddbc/FizzBuzzTest.java
@@ -4,8 +4,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FizzBuzzTest {
+
     @Test
-    void test() {
-        fail("これは失敗するはず");
+    public void _1を渡すと文字列1を返す() {
+        // 準備
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        // 実行 & 検証
+        assertEquals("1", fizzbuzz.convert(1));
+    }
+
+    @Test
+    public void _2を渡すと文字列2を返す() {
+        // 準備
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        // 実行 & 検証
+        assertEquals("2", fizzbuzz.convert(2));
     }
 }
