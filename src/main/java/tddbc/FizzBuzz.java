3092f7931a747cf4557733a3df0af55f7e2c070b
古川広一 <ex.kouichi1224@gmail.com>
2020-11-20 19:19:40 +0900

    FizzBuzzを実現するテストが完成

--- src/main/java/tddbc/FizzBuzz.java
+++ src/main/java/tddbc/FizzBuzz.java
@@ -5,6 +5,9 @@ package tddbc;
  */
 public class FizzBuzz {
     public String convert(int num) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            return "FizzBuzz";
+        }
         if (num % 3 == 0) {
             return "Fizz";
         }
--- src/test/java/tddbc/FizzBuzzTest.java
+++ src/test/java/tddbc/FizzBuzzTest.java
@@ -1,12 +1,15 @@
 package tddbc;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * FizzBuzz クラスをテストするためのクラス
  */
+@DisplayName("FizzBuzz 数列と変換規則を扱う FizzBuzz クラス")
 public class FizzBuzzTest {
     private FizzBuzz fizzbuzz;
 
@@ -15,23 +18,38 @@ public class FizzBuzzTest {
         fizzbuzz = new FizzBuzz();
     }
 
-    @Test
-    public void _1を渡すと文字列1を返す() {
-        assertEquals("1", fizzbuzz.convert(1));
-    }
+    @Nested
+    class convertメソッドは数を文字列に変換する {
+        @Nested
+        class _3の倍数のときは数の代わりにFizzに変換する {
+            @Test
+            public void _3を渡すと文字列Fizzを返す() {
+                assertEquals("Fizz", fizzbuzz.convert(3));
+            }
+        }
 
-    @Test
-    public void _2を渡すと文字列2を返す() {
-        assertEquals("2", fizzbuzz.convert(2));
-    }
+        @Nested
+        class _5の倍数のときは数の代わりにBuzzに変換する {
+            @Test
+            public void _5を渡すと文字列Buzzを返す() {
+                assertEquals("Buzz", fizzbuzz.convert(5));
+            }
+        }
 
-    @Test
-    public void _3を渡すと文字列Fizzを返す() {
-        assertEquals("Fizz", fizzbuzz.convert(3));
-    }
+        @Nested
+        class _3と5両方の倍数のときは数の代わりにFizzBuzzに変換する {
+            @Test
+            public void _15を渡すと文字列FizzBuzzを返す() {
+                assertEquals("FizzBuzz", fizzbuzz.convert(15));
+            }
+        }
 
-    @Test
-    public void _5を渡すと文字列Buzzを返す() {
-        assertEquals("Buzz", fizzbuzz.convert(5));
+        @Nested
+        class その他の数のときはそのまま文字列に変換する {
+            @Test
+            public void _1を渡すと文字列1を返す() {
+                assertEquals("1", fizzbuzz.convert(1));
+            }
+        }
     }
 }
