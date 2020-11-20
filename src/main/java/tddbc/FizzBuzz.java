f19400d4c6ddcf6fea7c66986508fd36153ceab1
古川広一 <ex.kouichi1224@gmail.com>
2020-11-20 09:37:53 +0900

    3の倍数のときFizz、5の倍数のときBuzz のテストと実装を追加

--- src/main/java/tddbc/FizzBuzz.java
+++ src/main/java/tddbc/FizzBuzz.java
@@ -5,6 +5,12 @@ package tddbc;
  */
 public class FizzBuzz {
     public String convert(int num) {
+        if (num % 3 == 0) {
+            return "Fizz";
+        }
+        if (num % 5 == 0) {
+            return "Buzz";
+        }
         return String.valueOf(num);
     }
 }
--- src/test/java/tddbc/FizzBuzzTest.java
+++ src/test/java/tddbc/FizzBuzzTest.java
@@ -1,23 +1,37 @@
 package tddbc;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * FizzBuzz クラスをテストするためのクラス
+ */
 public class FizzBuzzTest {
+    private FizzBuzz fizzbuzz;
+
+    @BeforeEach
+    void 前準備() {
+        fizzbuzz = new FizzBuzz();
+    }
 
     @Test
     public void _1を渡すと文字列1を返す() {
-        // 準備
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        // 実行 & 検証
         assertEquals("1", fizzbuzz.convert(1));
     }
 
     @Test
     public void _2を渡すと文字列2を返す() {
-        // 準備
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        // 実行 & 検証
         assertEquals("2", fizzbuzz.convert(2));
+    }
+
+    @Test
+    public void _3を渡すと文字列Fizzを返す() {
+        assertEquals("Fizz", fizzbuzz.convert(3));
+    }
+
+    @Test
+    public void _5を渡すと文字列Buzzを返す() {
+        assertEquals("Buzz", fizzbuzz.convert(5));
     }
 }
